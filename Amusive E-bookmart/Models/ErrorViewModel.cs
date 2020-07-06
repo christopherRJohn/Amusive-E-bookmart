737ace3f1627f65ab8c928bb15e42fd58f5deaa9
John <john7rocks@gmail.com>
2020-07-07 00:33:39 +0530

    Add project files.

---
+++ Amusive E-bookmart/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Amusive_E_bookmart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
